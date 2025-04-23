1a997df61bcc663707a729737efb08903be465a1
senkxff <xxzarenf@gmail.com>
2025-04-24 01:46:36 +0300

    Create functionality for ExitButton

--- Notes/View/UserControls/ExitButton.xaml.cs
+++ Notes/View/UserControls/ExitButton.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Notes.View.UserControls
 {
     /// <summary>
-    /// Логика взаимодействия для ExitButton.xaml
+    /// Logic of interapt for ExitButton.xaml
     /// </summary>
     public partial class ExitButton : UserControl
     {
         public ExitButton()
         {
             InitializeComponent();
+        }
+
+        private void _ExitButton_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
