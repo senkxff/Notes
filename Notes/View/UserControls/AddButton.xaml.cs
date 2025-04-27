6760c82e2509c1e653199ed698dd3b028b3d1488
senkxff <xxzarenf@gmail.com>
2025-04-27 23:46:15 +0300

    Create AddButton functionality

--- Notes/View/UserControls/AddButton.xaml.cs
+++ Notes/View/UserControls/AddButton.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Notes.View.UserControls
 {
@@ -7,9 +9,31 @@ namespace Notes.View.UserControls
     /// </summary>
     public partial class AddButton : UserControl
     {
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            "Command", 
+            typeof(ICommand), 
+            typeof(AddButton), 
+            new PropertyMetadata(null));
+
+
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
         public AddButton()
         {
             InitializeComponent();
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Command?.CanExecute(null) == true)
+            {
+                Command.Execute(null);
+            }
         }
     }
 }
