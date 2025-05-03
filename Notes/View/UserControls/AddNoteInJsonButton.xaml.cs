44d6c7ab31dfe65df1d433949c54970df6a212be
senkxff <xxzarenf@gmail.com>
2025-05-03 19:57:28 +0300

    Create Logic of Work with Google Drive

--- Notes/View/UserControls/AddNoteInJsonButton.xaml.cs
+++ Notes/View/UserControls/AddNoteInJsonButton.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Notes.View.UserControls
 {
@@ -7,9 +9,30 @@ namespace Notes.View.UserControls
     /// </summary>
     public partial class AddNoteInJsonButton : UserControl
     {
+        private static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            "Command",
+            typeof(ICommand),
+            typeof(AddNoteInJsonButton),
+            new PropertyMetadata(null)
+            );
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty); 
+            set => SetValue(CommandProperty, value); 
+        }
+
         public AddNoteInJsonButton()
         {
             InitializeComponent();
+        }
+
+        private void AddNoteInJsonBtn_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (Command?.CanExecute(null) == true)
+            {
+                Command?.Execute(null);
+            }
         }
     }
 }
--- Notes/ViewModel/NotesViewModel.cs
+++ Notes/ViewModel/NotesViewModel.cs
@@ -1,13 +1,19 @@
-﻿    using System.Collections.ObjectModel;
-    using System.ComponentModel;
-    using System.Runtime.CompilerServices;
-    using System.Windows;
-    using System.Windows.Input;
-    using Microsoft.Win32;
-    using System.Windows.Media.Imaging;
-    using Notes.Commands;
-    using Notes.Model;
-    using Notes.View.Windows.WarningWindows;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using Notes.Commands;
+using Notes.Model;
+using Notes.View.Windows.WarningWindows;
 
 namespace Notes.ViewModel
 {
@@ -72,6 +78,9 @@ namespace Notes.ViewModel
             AddImageCommand = new CommonCommand(AddImage);
             AddNoteCommand = new CommonCommand(AddNote);
             DeleteNoteCommand = new CommonCommand(DeleteNote);
+            SaveNoteCommand = new CommonCommand(async () => await Task.Run(() => SaveNoteAsync()));
+
+            Parallel.Invoke(() => LoadNotesAsync(), () => LoadImagesAsync());
 
             SelectedNote = notes[0];
         }
@@ -136,20 +145,189 @@ namespace Notes.ViewModel
             {
                 try
                 {
-                    BitmapImage bitmap = new BitmapImage();
-                    bitmap.BeginInit();
-                    bitmap.UriSource = new Uri(openFileDialog.FileName);
-                    bitmap.CacheOption = BitmapCacheOption.OnDemand;
-                    bitmap.EndInit();
-
+                    byte[] imageBytes = File.ReadAllBytes(openFileDialog.FileName);
+                    string bmpBase64 = Convert.ToBase64String(imageBytes);
                     if (SelectedNote != null)
                     {
+                        SelectedNote.ImagesBase64.Add(bmpBase64);
+                        BitmapImage bitmap = new BitmapImage();
+                        using (MemoryStream ms = new MemoryStream(imageBytes))
+                        {
+                            bitmap.BeginInit();
+                            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                            bitmap.StreamSource = ms;
+                            bitmap.EndInit();
+                            bitmap.Freeze();
+                        }
                         SelectedNote.Images.Add(bitmap);
                     }
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show($"Ошибка загрузки изображения: {ex.Message}");
+                }
+            }
+        }
+
+
+        public ICommand SaveNoteCommand { get; }
+        private bool isSyncing = false;
+        private async void SaveNoteAsync()
+        {
+            if (isSyncing)
+            {
+                return;
+            }
+            else
+            {
+                isSyncing = true;
+            }
+
+            try
+            {
+                string localFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NotesCollection.json");
+
+                string jsonNotes = JsonConvert.SerializeObject(notes, Formatting.Indented);
+                await File.WriteAllTextAsync(localFilePath, jsonNotes);
+
+                UserCredential credential;
+                using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = "token.json";
+                    credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.FromStream(stream).Secrets,
+                        new[] { DriveService.Scope.Drive },
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true));
+                }
+
+                var service = new DriveService(new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Notes"
+                });
+
+                var listRequest = service.Files.List();
+                listRequest.Q = "name = 'NotesCollection.json' and trashed = false";
+                listRequest.Spaces = "drive";
+                var fileList = await listRequest.ExecuteAsync();
+
+                string? fileId = fileList.Files != null && fileList.Files.Count > 0 ? fileList.Files.First().Id : null;
+
+                var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+                {
+                    Name = "NotesCollection.json"
+                };
+
+                using (var fileStream = new FileStream(localFilePath, FileMode.Open))
+                {
+                    if (fileId == null)
+                    {
+                        var createRequest = service.Files.Create(fileMetadata, fileStream, "application/json");
+                        await createRequest.UploadAsync();
+                        fileId = createRequest.ResponseBody.Id;
+                    }
+                    else
+                    {
+                        var updateRequest = service.Files.Update(fileMetadata, fileId, fileStream, "application/json");
+                        await updateRequest.UploadAsync();
+                    }
+                }
+
+                if (fileId != null)
+                {
+                    var downloadRequest = service.Files.Get(fileId);
+                    var memoryStream = new MemoryStream();
+                    await downloadRequest.DownloadAsync(memoryStream);
+                    memoryStream.Position = 0;
+                    using (var reader = new StreamReader(memoryStream))
+                    {
+                        string downloadedJson = await reader.ReadToEndAsync();
+                        var updatedNotes = JsonConvert.DeserializeObject<ObservableCollection<NoteModel>>(downloadedJson);
+
+                        if (updatedNotes != null)
+                        {
+                            Application.Current.Dispatcher.Invoke(() =>
+                            {
+                                notes.Clear();
+                                foreach (var note in updatedNotes)
+                                {
+                                    notes.Add(note);
+                                }
+                            });
+                        }
+                    }
+                }
+                await Task.Run(() => LoadImagesAsync());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Произошло исключение {e.Message}! Пожалуйста, перезапустите программу и сообщите о проблеме разработчику!");
+            }
+            finally
+            {
+                isSyncing = false;
+            }
+        }
+
+        private async Task LoadNotesAsync()
+        {
+            string localFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NotesCollection.json");
+
+            if (File.Exists(localFilePath))
+            {
+                string json = await File.ReadAllTextAsync(localFilePath);
+                var loadedNotes = JsonConvert.DeserializeObject<ObservableCollection<NoteModel>>(json);
+
+                if (loadedNotes != null)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        notes.Clear();
+                        foreach (var note in loadedNotes)
+                        {
+                            notes.Add(note);
+                        }
+                    });
+                }
+            }
+        }
+
+        private async Task LoadImagesAsync()
+        {
+            string localFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "NotesCollection.json");
+
+            if (File.Exists(localFilePath))
+            {
+                string json = await File.ReadAllTextAsync(localFilePath);
+                var loadedNotes = JsonConvert.DeserializeObject<ObservableCollection<NoteModel>>(json);
+                if (loadedNotes != null)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        notes.Clear();
+                        foreach (var note in loadedNotes)
+                        {
+                            // Восстанавливаем изображения из Base64-строк.
+                            note.Images.Clear();
+                            foreach (var base64 in note.ImagesBase64)
+                            {
+                                BitmapImage image = new BitmapImage();
+                                byte[] imageBytes = Convert.FromBase64String(base64);
+                                using (MemoryStream ms = new MemoryStream(imageBytes))
+                                {
+                                    image.BeginInit();
+                                    image.CacheOption = BitmapCacheOption.OnLoad;
+                                    image.StreamSource = ms;
+                                    image.EndInit();
+                                    image.Freeze();
+                                }
+                                note.Images.Add(image);
+                            }
+                            notes.Add(note);
+                        }
+                    });
                 }
             }
         }
